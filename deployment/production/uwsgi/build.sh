d30e6155314272c1bd0b8469ab155d95392fbcac
Dimas <dimas.ciputra@gmail.com>
2018-10-23 16:48:43 +0700

    Update build configuration

--- deployment/production/uwsgi/build.sh
+++ deployment/production/uwsgi/build.sh
@@ -22,12 +22,16 @@ if [ -z "$HEALTHYRIVERS_TAG" ]; then
 	HEALTHYRIVERS_TAG=develop
 fi
 
+if [ -z "$BIMS_TAG" ]; then
+	BIMS_TAG=latest
+fi
+
 echo "HEALTHYRIVERS_TAG=${HEALTHYRIVERS_TAG}"
 
 echo "Build: $REPO_NAME/$IMAGE_NAME:$TAG_NAME"
 
 docker build -t ${REPO_NAME}/${IMAGE_NAME} \
-	--build-arg HEALTHYRIVERS_TAG=${HEALTHYRIVERS_TAG} \
+	--build-arg HEALTHYRIVERS_TAG=${HEALTHYRIVERS_TAG} --build-arg BIMS_TAG=${BIMS_TAG} \
 	${BUILD_ARGS} .
 docker tag ${REPO_NAME}/${IMAGE_NAME}:latest ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
 docker push ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
